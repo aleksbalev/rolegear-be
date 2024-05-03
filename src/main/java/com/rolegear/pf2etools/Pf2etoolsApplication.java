89f1d59e93e836ec75be180be64d5474bb1c22df
Alex Balev <aleks.balev7899@gmail.com>
2024-05-03 21:10:19 +0200

    project init

---
+++ src/main/java/com/rolegear/pf2etools/Pf2etoolsApplication.java
@@ -0,0 +1,13 @@
+package com.rolegear.pf2etools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pf2etoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pf2etoolsApplication.class, args);
+	}
+
+}
